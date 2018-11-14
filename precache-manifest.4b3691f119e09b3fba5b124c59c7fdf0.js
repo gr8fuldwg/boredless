c314c7745f7c90e1e83393484d30bc689d46cae9
JON J <gr8fuldwg@gmail.com>
2018-11-13 20:55:10 -0500

    Updates

---
+++ precache-manifest.4b3691f119e09b3fba5b124c59c7fdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a19f666392594a41151",
+    "url": "/boredless/static/js/runtime~main.4a19f666.js"
+  },
+  {
+    "revision": "3b77ddf0159d61ef6c3d",
+    "url": "/boredless/static/js/main.3b77ddf0.chunk.js"
+  },
+  {
+    "revision": "055f505e627948fce168",
+    "url": "/boredless/static/js/1.055f505e.chunk.js"
+  },
+  {
+    "revision": "3b77ddf0159d61ef6c3d",
+    "url": "/boredless/static/css/main.3a5f1d0c.chunk.css"
+  },
+  {
+    "revision": "055f505e627948fce168",
+    "url": "/boredless/static/css/1.56c88c1c.chunk.css"
+  },
+  {
+    "revision": "f83009f8308592a6978647882e2b7795",
+    "url": "/boredless/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/boredless/precache-manifest.4b3691f119e09b3fba5b124c59c7fdf0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/boredless/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
