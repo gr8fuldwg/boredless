353afd9ab7bba61c946b03ff645ee49027bcb9ac
JON J <gr8fuldwg@gmail.com>
2018-11-14 19:52:48 -0500

    updated to text for development vs production for the bored api url

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import axios from 'axios';
 const appElement = document.getElementById('root');
 Modal.setAppElement(appElement);
 
+const isDev = process.env.NODE_ENV !== 'production';
 // BORED API stuffs 
-const BORED_API = '/activity';
-
+const BORED_API = isDev ? '/activity' : 'https://boredapi.com/activity';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modalOpen: false,
-      activity: ''
+      activity: '',
+      error: null
     }
     this.handleModalOpen = this.handleModalOpen.bind(this);
     this.handleModalClose = this.handleModalClose.bind(this);
@@ -25,35 +26,43 @@ class App extends Component {
       .then(response => {
         const { activity } = response.data;
         this.setState({
-           modalOpen: true,
-           activity: activity
-           });
+          modalOpen: true,
+          activity: activity,
+          error: null
+        });
       })
-      .catch(err => {});
-    
+      .catch(err => {
+        this.setState({
+          error: err
+        });
+      });
+
   }
   handleModalClose() {
     this.setState({ modalOpen: false });
   }
   render() {
     return (
-      <div className="vh-100  bg-green flex justify-center items-center">
+      <div className="vh-100  bg-green flex-column flex justify-center items-center">
+        {this.state.error &&
+          <div className="red bg-black f4 mb4 h3 w5" >
+            Error: {this.state.error.message} </div>}
         <button className="f1 b--none outline-transparent br-100 h5 w5 dim ph3 pv2 mb2 dib white bg-black"
           onClick={this.handleModalOpen}>
           Bored?
         </button>
         <Modal closeTimeoutMS={150} isOpen={this.state.modalOpen}>
-        <div className="flex flex-column h-100" >
-          <header className="flex justify-end">
-            <button className="f1 ph3 pv2 mb2 dib white bg-black b--none" onClick={this.handleModalClose}>X</button>
-          </header>
-          <main className="flex-grow-1 flex flex-column justify-center items-center">
-            <h1>Here is something you can do...</h1>
-            <p>{this.state.activity}</p>
-          </main>
-          <footer>
-            <h2>Nothin to see here.</h2>
-          </footer>
+          <div className="flex flex-column h-100" >
+            <header className="flex justify-end">
+              <button className="f1 ph3 pv2 mb2 dib white bg-black b--none" onClick={this.handleModalClose}>X</button>
+            </header>
+            <main className="flex-grow-1 flex flex-column justify-center items-center">
+              <h1>Here is something you can do...</h1>
+              <p>{this.state.activity}</p>
+            </main>
+            <footer>
+              <h2>Nothin to see here.</h2>
+            </footer>
           </div>
         </Modal>
       </div>
